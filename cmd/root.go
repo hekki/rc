608f4ae40b3765b866e90841d6754d7e1af7828b
Hiroki Ito <contact@hekki.info>
2019-09-15 12:18:38 +0900

    Supports binay and hex conversion

--- cmd/root.go
+++ cmd/root.go
@@ -9,6 +9,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var binary bool
+var hex bool
+
+func init() {
+	RootCmd.PersistentFlags().BoolVarP(&binary, "binary", "b", false, "binary flag")
+	RootCmd.PersistentFlags().BoolVarP(&hex, "hex", "e", false, "hex flag")
+}
+
 // RootCmd is root command
 var RootCmd = &cobra.Command{
 	Use:   "rc [num]",
@@ -23,15 +31,42 @@ var RootCmd = &cobra.Command{
 		return nil
 	},
 	Run: func(cmd *cobra.Command, args []string) {
-		rowNum, err := strconv.Atoi(args[0])
-		if err != nil {
-			log.Fatalf("%s is not a decimal number.", args[0])
+		var rowNum int
+
+		if binary {
+			bRowNum, err := strconv.ParseInt(args[0], 2, 64)
+			if err != nil {
+				log.Fatalf("%s is not a binary number.", args[0])
+			}
+
+			rowNum = int(bRowNum)
+		} else if hex {
+			hRowNum, err := strconv.ParseInt(args[0], 16, 64)
+			if err != nil {
+				log.Fatalf("%s is not a hex number.", args[0])
+			}
+
+			rowNum = int(hRowNum)
+		} else {
+			dRowNum, err := strconv.Atoi(args[0])
+			if err != nil {
+				log.Fatalf("%s is not a decimal number.", args[0])
+			}
+
+			rowNum = dRowNum
 		}
 
-		binaryNum := fmt.Sprintf("Binary: %b", rowNum)
-		fmt.Println(binaryNum)
-
-		hexNum := fmt.Sprintf("Hex: %x", rowNum)
-		fmt.Println(hexNum)
+		formatPrint(rowNum)
 	},
+}
+
+func formatPrint(d int) {
+	decimalNum := fmt.Sprintf("Decimal: %d", d)
+	fmt.Println(decimalNum)
+
+	binaryNum := fmt.Sprintf("Binary: %b", d)
+	fmt.Println(binaryNum)
+
+	hexNum := fmt.Sprintf("Hex: %x", d)
+	fmt.Println(hexNum)
 }
